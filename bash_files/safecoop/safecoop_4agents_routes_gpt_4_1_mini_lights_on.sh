e4c12c00fef5144b95ff23c29100c03648f1b23d
xiangbogaobarry <xiangbogaobarry@gmail.com>
2025-08-26 16:25:02 -0500

    Update: some new config files for different version of gpts

---
+++ bash_files/safecoop/safecoop_4agents_routes_gpt_4_1_mini_lights_on.sh
@@ -0,0 +1,8 @@
+Route_id=1
+Carla_port=2000
+Method_tag="safecoop_4agents_gpt_4_1_mini_lights_on"
+Repeat_id=0
+Agent_config="safecoop/safecoop_4agents_gpt_4_1_mini"
+Scenario_config="1_lights_on"
+
+CUDA_VISIBLE_DEVICES=0 bash scripts/eval_driving_vlm.sh ${Route_id} ${Carla_port} ${Method_tag} ${Repeat_id} ${Agent_config} ${Scenario_config}
