a92ee8643c08e9b2727141284331f29058bcb6da
xiangbogaobarry <xiangbogaobarry@gmail.com>
2025-08-26 16:53:00 -0500

    change to use gpt 4.1 mini by default

--- bash_files/safecoop/safecoop_4agents.sh
+++ bash_files/safecoop/safecoop_4agents.sh
@@ -3,6 +3,6 @@ Carla_port=2000
 Method_tag="safecoop_4agents"
 Repeat_id=0
 Agent_config="safecoop/safecoop_4agents"
-Scenario_config="1"
+Scenario_config="1_lights_on"
 
 CUDA_VISIBLE_DEVICES=0 bash scripts/eval_driving_vlm.sh ${Route_id} ${Carla_port} ${Method_tag} ${Repeat_id} ${Agent_config} ${Scenario_config}
